c1784ab53f700f19dcaf425cb411c48b6d29429e
Hernán Sosa <hernan.sosa@25watts.com.ar>
2025-04-29 11:05:28 -0300

    Display products field when woocommerces is activated

--- admin/partials/auto-delete-images-admin-display.php
+++ admin/partials/auto-delete-images-admin-display.php
@@ -20,7 +20,7 @@
 </style>
 
 <div class=" flex-column gap-s bg-dark-grey-2 text-white padding-l">
-<img class="max-width-10" src="https://helloeveryone.me/wp-content/uploads/HelloEveryone-Logo-300x79.webp" alt="Logo Hello Everyone">	
+	<img class="max-width-10" src="https://helloeveryone.me/wp-content/uploads/HelloEveryone-Logo-300x79.webp" alt="Logo Hello Everyone">
 	<address class="text-xs flex-row items-middle gap-xs">
 		Desarrollado por&nbsp;<a class="text-white transition-global" href="https://helloeveryone.me" rel="noreferrer" target="_blank">Hello Everyone</a>
 	</address>
@@ -29,12 +29,16 @@
 	<h1 class="text-l font-700"> Opciones de <span class="underline">Auto-Delete Images</span></h1>
 	<hr class="margin-vertical-m">
 	<form method="post" action="options.php">
-		<p class="text-m">¿Activar funcionalidad para productos?</p>
-		<div class="flex-row gap-4xs items-middle margin-top-s">
-			<input class="margin-auto" type="checkbox" name="_activate_for_products" id="_activate_for_products" value="1" <?php checked(get_option('_activate_for_products'), 1); ?>>
-			<label for="_activate_for_products" class="text-s">Activar en productos</label>
-		</div>
-		<hr class="margin-vertical-m">
+		<?php if (class_exists('WooCommerce')) { ?>
+			<p class="text-m">¿Activar funcionalidad para productos?</p>
+			<div class="flex-row gap-4xs items-middle margin-top-s">
+				<input class="margin-auto" type="checkbox" name="_activate_for_products" id="_activate_for_products" value="1" <?php checked(get_option('_activate_for_products'), 1); ?>>
+				<label for="_activate_for_products" class="text-s">Activar en productos</label>
+			</div>
+			<hr class="margin-vertical-m">
+			<?php
+			}
+		?>
 		<p class="text-m">¿Activar funcionalidad para posts?</p>
 		<div class="flex-row gap-4xs items-middle margin-top-s">
 			<input type="checkbox" name="_activate_for_posts" id="_activate_for_posts" value="1" <?php checked(get_option('_activate_for_posts'), 1); ?>>
@@ -48,9 +52,9 @@
 		</div>
 		<hr class="margin-vertical-m">
 		<?php
-			settings_fields('auto_delete_images_options_group');
-			do_settings_sections('auto-delete-images');
-			submit_button();
+		settings_fields('auto_delete_images_options_group');
+		do_settings_sections('auto-delete-images');
+		submit_button();
 		?>
 	</form>
 </div>
